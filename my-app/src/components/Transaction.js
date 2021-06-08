4aad640e6d3d5490707cda43f5aa367c94c2b6f5
StephensGit <stephenmoran@live.ie>
2021-06-08 10:26:17 +0100

    delete method functionality added
    Als

--- my-app/src/components/Transaction.js
+++ my-app/src/components/Transaction.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
+    const { deleteTransaction } = useContext(GlobalContext);
     const {text, amount} = transaction;
-    const sign = amount < 0 ? '-' : '+';
+    const sign = transaction.amount < 0 ? '-' : '+';
     
     return (
         <li className={ amount < 0 ? 'minus' : 'plus' }>
-            { text } <span>{ sign }${Math.abs (amount) }</span><button className="delete-btn">x</button>
+            { text } <span>{ sign }${Math.abs (amount) }</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
         </li>
     )
 }
--- my-app/src/context/GlobalState.js
+++ my-app/src/context/GlobalState.js
@@ -16,9 +16,17 @@ export const GlobalContext = createContext(initialState);
 // Create Provider component
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
-
+    // Actions
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    };
+    
     return (<GlobalContext.Provider value={{
-        transactions:state.transactions
+        transactions:state.transactions,
+        deleteTransaction
     }}>
         {children}
     </GlobalContext.Provider>)
