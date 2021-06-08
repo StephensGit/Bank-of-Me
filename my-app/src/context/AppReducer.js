bfaa21b2be869a18b42f795caf0cdef2750853a4
StephensGit <stephenmoran@live.ie>
2021-06-08 14:27:11 +0100

    Finance Tracker completed

--- my-app/src/context/AppReducer.js
+++ my-app/src/context/AppReducer.js
@@ -5,6 +5,12 @@ export default (state, action) => {
                     ...state,
                     transactions: state.transactions.filter(transaction => transaction.id  !== action.payload)
                 }
+        case 'ADD_TRANSACTION':
+                return {
+                    ...state,
+                    // ...state gets a copy of the current state, action.payload is the new transaction we're adding,and we add it to state.transactions array
+                    transactions: [action.payload, ...state.transactions]
+                }
         default:
             return state;
     }
